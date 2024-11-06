4a6271642fbbd182182eaca40adf3496d626bd94
fireyork <andrre2001@gmail.com>
2024-11-06 11:49:03 +0300

    first step

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     application
     checkstyle
     jacoco
+    id("io.freefair.lombok") version "8.6"
 }
 
 group = "hexlet.code"
@@ -17,12 +18,32 @@ repositories {
 }
 
 dependencies {
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("gg.jte:jte:3.1.12")
+    implementation("io.javalin:javalin-bundle:6.1.3")
+    implementation("com.h2database:h2:2.2.220")
+    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation ("org.postgresql:postgresql:42.2.23")
+    implementation ("com.konghq:unirest-java:3.11.09")
+    implementation ("org.jsoup:jsoup:1.14.3")
+    implementation("net.datafaker:datafaker:2.0.1")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.23.1")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
+    testImplementation("org.mockito:mockito-core:5.14.2")
+    testImplementation ("org.mockito:mockito-inline:4.0.0")
 }
 
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required = true
+    }
 }
 
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,34 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import java.io.IOException;
+import java.sql.SQLException;
+import io.javalin.rendering.template.JavalinJte;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+    public static void main(String[] args) throws SQLException, IOException {
+        var app = getApp();
+        app.start(getPort());
+    }
+
+    public static Javalin getApp() throws IOException, SQLException {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.get("/", ctx -> ctx.result("Hello World!"));
+
+        log.atError();
+
+        return app;
+    }
+
+    public static int getPort() {
+        var port = System.getenv().getOrDefault("DB_PORT", "7070");
+        return Integer.valueOf(port);
+    }
+
+}
