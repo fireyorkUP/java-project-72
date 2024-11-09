ddd2c6c3669cac903bb9ed10921f04a89033ec8e
fireyork <andrre2001@gmail.com>
2024-11-09 14:43:28 +0300

    third step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,10 @@
 package hexlet.code;
 
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.resolve.ResourceCodeResolver;
+import hexlet.code.controller.RootController;
+import hexlet.code.util.NamedRoutes;
 import io.javalin.Javalin;
 import java.io.IOException;
 import java.sql.SQLException;
@@ -21,9 +26,10 @@ public class App {
         }
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
-            config.fileRenderer(new JavalinJte());
+            config.fileRenderer(new JavalinJte(createTemplateEngine()));
         });
-        app.get("/", ctx -> ctx.result("Hello World!"));
+
+        app.get(NamedRoutes.rootPath(), RootController::index);
 
         log.atError();
 
@@ -33,6 +39,13 @@ public class App {
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "7070");
         return Integer.valueOf(port);
+    }
+
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
+        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+        return templateEngine;
     }
 
 }
---
+++ app/src/main/java/hexlet/code/dto/url/BasePage.java
@@ -0,0 +1,11 @@
+package hexlet.code.dto.url;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BasePage {
+    private String flash;
+    private String flashType;
+}
